10f7165a05dd1c47bb6af693b85aa6ce7856ec63
Omar Ahmed <oa109115@gmail.com>
2026-05-09 20:14:30 +0300

    f

--- eda_analysis.py
+++ eda_analysis.py
@@ -8,16 +8,16 @@ def analyze_attrition_distribution(df):
 def analyze_salary_vs_performance(df):
     """Compare average monthly income based on performance ratings."""
     return df.groupBy("PerformanceRating") \
-             .agg(F.round(F.avg("MonthlyIncome"), 2).alias("Average_Monthly_Income")) \
-             .orderBy("PerformanceRating")
+                .agg(F.round(F.avg("MonthlyIncome"), 2).alias("Average_Monthly_Income")) \
+                .orderBy("PerformanceRating")
 
 def analyze_department_insights(df):
     """Extract average income and attrition rate per department."""
     dept_insights = df.groupBy("Department") \
-                      .agg(
-                          F.count("EmployeeNumber").alias("Total_Employees"),
-                          F.round(F.avg("MonthlyIncome"), 2).alias("Avg_Income"),
-                          F.sum(F.when(F.col("Attrition") == "Yes", 1).otherwise(0)).alias("Attrition_Count")
-                      )
+                        .agg(
+                            F.count("EmployeeNumber").alias("Total_Employees"),
+                            F.round(F.avg("MonthlyIncome"), 2).alias("Avg_Income"),
+                            F.sum(F.when(F.col("Attrition") == "Yes", 1).otherwise(0)).alias("Attrition_Count")
+                        )
     return dept_insights.withColumn("Attrition_Rate_%", 
                                    F.round((F.col("Attrition_Count") / F.col("Total_Employees")) * 100, 2))
